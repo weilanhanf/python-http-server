eaea331be49d0f559b108e0f465473ed6ad59e18
weilanhanf <weilanhanf@163.com>
2020-02-09 17:22:02 +0800

    add handler server

---
+++ handler/base_handler.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+__date__ = '2020/2/9 15:59'
+
+
+class BaseRequestHandler:
+    """ 请求处理基类：定义基本属性接口 """
+
+    def __init__(self, server_socket=None, request=None, client_address=None):
+        self.server_socket = server_socket
+        self.request = request
+        self.client_address = client_address
+
+    def handle(self):
+        raise Exception("This method need be overridden")
+
+
+class StreamRequestHandler(BaseRequestHandler):
+    """ 网络请求处理器类：封装TCP连接处理逻辑，编码解码，读写消息"""
+
+    def __init__(self, server_socket=None, request=None, client_address=None):
+        super().__init__(server_socket, request, client_address)
+        self.buff = []
+        # 把连接请求分离为读和写文件描述符
+        self.readfile = self.request.makefile("rb")
+        self.writefile = self.request.makefile("wb")
+
+    # 编码：字符串->字节流
+    def encode(self, msg=None):
+        if not isinstance(msg, bytes):  # 如果不是字节码，编码为字节码
+            msg = bytes(msg, encoding='utf-8')
+        return msg
+
+    # 解码：字节流->字符串
+    def decode(self, msg=None):
+        if isinstance(msg, bytes):  # 把字节码解码为字符串
+            msg = msg.decode()
+        return msg
+
+    # 读消息
+    def read(self, length=0):
+        msg = self.readfile.read(length)  # 指定读取一定长度的消息
+        return self.decode(msg)  # 返回解码后的字符串
+
+    # 读取一行消息
+    def read_line(self, length=65536):  # 浏览器能够处理的请求的大小默认值length
+        msg = self.readfile.read(length).strip()
+        return self.decode(msg)
+
+    # 写消息
+    def write_content(self, msg):
+        msg = self.encode(msg)
+        self.buff.append(msg)
+
+    def send(self):
+        for line in self.buff:
+            self.writefile.write(line)
+        self.writefile.flush()  # 所有缓存数据强发送到目的地
+        self.buff = []  # 缓存区清空
+
+    # 关闭文件描述符
+    def close(self):
+        self.request.readfile.close()
+        self.request.writefile.close()
---
+++ server/socket_server.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+__date__ = '2020/2/9 12:00'
+# https://docs.python.org/zh-cn/3/library/socket.html
+
+
+import socket
+
+
+class TCPSerer:
+    """ 接受客户端的TCP连接 """
+
+    def __init__(self, server_address=None, handler_class=None):
+        self.server_address = server_address  # 服务端地址
+        self.HandlerClass = handler_class  # 网络请求处理器类
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # 套接字
+        # AF_INET表示一个地址族(ip, 端口号)  SOCK_STREAM表示接收字节流
+        self.is_shutdown = False  # 服务器状态，默认停机
+
+    # 服务器启动函数
+    def server_forever(self):
+        self.server_socket.bind(self.server_address)  # 绑定端口号
+        self.server_socket.listen(10)  # 设置最大连接数，超出排队
+        # while True:
+        while self.is_shutdown is not False:
+            # 1. 接受请求
+            request, client_class = self.get_request()
+            # 2. 处理请求
+            try:  # 捕获处理数据中出现的异常，避免服务器宕机
+                self.process_request(request, client_class)
+            except Exception as e:
+                print(e)  # 控制台打印查看错误
+            finally:
+                # 3. 关闭连接
+                self.close_request(request)
+
+    # 接收请求
+    def get_request(self):
+        return self.server_socket.accept()
+
+    # 处理请求
+    def process_request(self, request=None, client_class=None):
+        handler = self.HandlerClass(request, client_class)  # 处理请求类
+        handler.handle()
+
+    # 关闭连接
+    def close_request(self, request=None):
+        request.close()
+        request.shutdown()
+
+    # 关闭服务器
+    def shutdown(self):
+        self.is_shutdown = True
